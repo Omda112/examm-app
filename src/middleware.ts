61461ab8fcbe3601a92552eff88c282aff6473f9
Omda112 <emadehab467@gmail.com>
2025-09-08 14:14:25 +0300

    l-15

--- src/middleware.ts
+++ src/middleware.ts
@@ -36,5 +36,8 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api/auth|_next/static|_next/image|favicon.ico).*)",
+  ],
 };
+
